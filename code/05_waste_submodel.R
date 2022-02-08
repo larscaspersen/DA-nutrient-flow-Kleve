7b99f6bc8e15c25871d56bc1922ac224709f5013
larscaspersen <larscaspersen@outlook.de>
2022-02-08 16:12:01 +0100

    updated variables function call

--- code/05_waste_submodel.R
+++ code/05_waste_submodel.R
@@ -2,7 +2,7 @@
 # 
 # library(decisionSupport)
 # 
-# waste_input <- read.csv('data/input-waste.csv') 
+# waste_input <- read.csv('data/input-all.csv')
 # 
 # make_variables<-function(est,n=1)
 # { x<-random(rho=est, n=n)
@@ -14,21 +14,27 @@
 # waste_input$upper <- as.numeric(waste_input$upper)
 # 
 # #create variable so I can test the function
-# #make_variables(as.estimate(waste_input),n=1)
+# make_variables(as.estimate(waste_input),n=1)
 
 ####wastewater
 
 waste_function <- function( waste_water,
                             N_content_wastewater,
+                            P_content_wastewater,
+                            K_content_wastewater,
                             lossrate_wastewater,
-                            share_N_sewage_for_agriculture,
+                            share_sewage_for_agriculture,
                             compost_to_horticulture,
                             compost_to_export,
                             compost_to_consumption,
                             dm_compost_consumption,
                             dm_compost_horticulture,
                             N_content_compost_consumption,
+                            P_content_compost_consumption,
+                            K_content_compost_consumption,
                             N_content_compost_horticulture,
+                            P_content_compost_horticulture,
+                            K_content_compost_horticulture,
                             ofmsw_import,
                             ofmsw_local,
                             green_waste_import,
@@ -38,9 +44,17 @@ waste_function <- function( waste_water,
                             dm_green_waste,
                             dm_ofmsw,
                             N_content_ofmsw_waste,
-                            N_content_green_waste
+                            P_content_ofmsw_waste,
+                            K_content_ofmsw_waste,
+                            N_content_green_waste,
+                            P_content_green_waste,
+                            K_content_green_waste
   
 ){
+  
+  ########
+  # SEWAGE
+  ########
   
   
   #transform wastewater from m3 to l
@@ -49,44 +63,140 @@ waste_function <- function( waste_water,
   #get N of waste water (= stream from consumption to waste in form of sewage)
   #N content is in mg per L but I need kg as output, so devide by 1000 * 1000
   N_sewage_in <- waste_water * N_content_wastewater / 1000000 
+  P_sewage_in <- waste_water * P_content_wastewater / 1000000 
+  K_sewage_in <- waste_water * K_content_wastewater / 1000000 
   
   #get amount of recovered and lost N by wastewater treatment
-  N_sewage_lost <- N_sewage_in * lossrate_wastewater
+  N_sewage_lost <- N_sewage_in * lossrate_N_wastewater
+  P_sewage_lost <- P_sewage_in * lossrate_P_wastewater
+  K_sewage_lost <- K_sewage_in * lossrate_K_wastewater
+  
   N_sewage_kept <- N_sewage_in - N_sewage_lost
+  P_sewage_kept <- P_sewage_in - P_sewage_lost
+  K_sewage_kept <- K_sewage_in - K_sewage_lost
+  
+  #the N / P / K sewage_kept represents the sludge which was extracted
+  #but when deciding how much N from sewage goes to agriculture, it is rather a X% of the sewage goes to agriculture
+  #and this amount X of sewage contains Y amount of N
+  #so I should use a N concentration to get total amount of sewage
+  #and then use a variable 
+  
   
   #get share of sewage N which is recycle back to field
-  N_sewage_to_crop <- N_sewage_kept * share_N_sewage_for_agriculture
+  N_sewage_to_crop <- N_sewage_kept * share_sewage_for_agriculture
+  P_sewage_to_crop <- P_sewage_kept * share_sewage_for_agriculture
+  K_sewage_to_crop <- K_sewage_kept * share_sewage_for_agriculture
+  
+  
   N_sewage_exported <- N_sewage_kept - N_sewage_to_crop
+  P_sewage_exported <- P_sewage_kept - P_sewage_to_crop
+  K_sewage_exported <- K_sewage_kept - K_sewage_to_crop
   
   
   
-  ####compost
+  
+  ######
+  # COMPOST
+  ######
+  
   N_compost_crop <- compost_to_horticulture * dm_compost_horticulture * N_content_compost_horticulture
+  P_compost_crop <- compost_to_horticulture * dm_compost_horticulture * P_content_compost_horticulture
+  K_compost_crop <- compost_to_horticulture * dm_compost_horticulture * K_content_compost_horticulture * convert_potassium_oxide_to_k
+  
   N_compost_export <- compost_to_export * dm_compost_horticulture * N_content_compost_horticulture
+  P_compost_export <- compost_to_export * dm_compost_horticulture * P_content_compost_horticulture
+  K_compost_export <- compost_to_export * dm_compost_horticulture * K_content_compost_horticulture * convert_potassium_oxide_to_k
+  
   N_compost_consumption <- compost_to_consumption * dm_compost_consumption * N_content_compost_consumption
+  P_compost_consumption <- compost_to_consumption * dm_compost_consumption * P_content_compost_consumption
+  K_compost_consumption <- compost_to_consumption * dm_compost_consumption * K_content_compost_consumption * convert_potassium_oxide_to_k
+  
+  
+  
+  
+  
+  ######
+  # SOLID WASTE
+  ######
   
   
   ####ofmsw waste
   N_ofmsw_local <- ofmsw_local * dm_ofmsw * N_content_ofmsw_waste
+  P_ofmsw_local <- ofmsw_local * dm_ofmsw * P_content_ofmsw_waste
+  K_ofmsw_local <- ofmsw_local * dm_ofmsw * K_content_ofmsw_waste
+  
   N_ofmsw_import <- ofmsw_import * dm_ofmsw * N_content_ofmsw_waste
+  P_ofmsw_import <- ofmsw_import * dm_ofmsw * P_content_ofmsw_waste
+  K_ofmsw_import <- ofmsw_import * dm_ofmsw * K_content_ofmsw_waste
+  
   N_green_waste_local <- green_waste_local * dm_green_waste * N_content_green_waste
+  P_green_waste_local <- green_waste_local * dm_green_waste * P_content_green_waste
+  K_green_waste_local <- green_waste_local * dm_green_waste * K_content_green_waste
+  
   N_green_waste_import <- green_waste_import * dm_green_waste * N_content_green_waste
+  P_green_waste_import <- green_waste_import * dm_green_waste * P_content_green_waste
+  K_green_waste_import <- green_waste_import * dm_green_waste * K_content_green_waste
+  
   N_grey_bin_food_waste <- grey_bin_food_waste * dm_ofmsw * N_content_ofmsw_waste
+  P_grey_bin_food_waste <- grey_bin_food_waste * dm_ofmsw * P_content_ofmsw_waste
+  K_grey_bin_food_waste <- grey_bin_food_waste * dm_ofmsw * K_content_ofmsw_waste
+  
   N_grey_bin_garden_waste <- grey_bin_garden_waste * dm_green_waste * N_content_green_waste
+  P_grey_bin_garden_waste <- grey_bin_garden_waste * dm_green_waste * P_content_green_waste
+  K_grey_bin_garden_waste <- grey_bin_garden_waste * dm_green_waste * K_content_green_waste
+  
   
   return(list(N_sewage_in = N_sewage_in,
+              P_sewage_in = P_sewage_in,
+              K_sewage_in = K_sewage_in,
+              
               N_sewage_lost = N_sewage_lost,
+              P_sewage_lost = P_sewage_lost,
+              K_sewage_lost = K_sewage_lost,
+              
               N_sewage_to_crop = N_sewage_to_crop,
+              P_sewage_to_crop = P_sewage_to_crop,
+              K_sewage_to_crop = K_sewage_to_crop,
+              
               N_sewage_exported = N_sewage_exported,
+              P_sewage_exported = P_sewage_exported,
+              K_sewage_exported = K_sewage_exported,
+              
               N_compost_crop = N_compost_crop,
+              P_compost_crop = P_compost_crop,
+              K_compost_crop = K_compost_crop,
+              
               N_compost_consumption = N_compost_consumption,
+              P_compost_consumption = P_compost_consumption,
+              K_compost_consumption = K_compost_consumption,
+              
               N_compost_export = N_compost_export,
+              P_compost_export = P_compost_export,
+              K_compost_export = K_compost_export,
+              
               N_ofmsw_local = N_ofmsw_local,
+              P_ofmsw_local = P_ofmsw_local,
+              K_ofmsw_local = K_ofmsw_local,
+              
               N_ofmsw_import = N_ofmsw_import,
+              P_ofmsw_import = P_ofmsw_import,
+              K_ofmsw_import = K_ofmsw_import,
+              
               N_green_waste_import = N_green_waste_import,
+              P_green_waste_import = P_green_waste_import,
+              K_green_waste_import = K_green_waste_import,
+              
               N_green_waste_local = N_green_waste_local,
+              P_green_waste_local = P_green_waste_local,
+              K_green_waste_local = K_green_waste_local,
+              
               N_grey_bin_garden_waste = N_grey_bin_garden_waste,
-              N_grey_bin_food_waste = N_grey_bin_food_waste))
+              P_grey_bin_garden_waste = P_grey_bin_garden_waste,
+              K_grey_bin_garden_waste = K_grey_bin_garden_waste,
+              
+              N_grey_bin_food_waste = N_grey_bin_food_waste,
+              P_grey_bin_food_waste = P_grey_bin_food_waste,
+              K_grey_bin_food_waste = K_grey_bin_food_waste))
   
   
 }
